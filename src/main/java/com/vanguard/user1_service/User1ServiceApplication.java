a1e18de924b633549c2ef9830e6d0024b9e2045f
abhijeet <abhijeet.thakur1@gmail.com>
2026-01-24 12:29:55 +0530

    chore: initial user-service setup

---
+++ src/main/java/com/vanguard/user1_service/User1ServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vanguard.user1_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class User1ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(User1ServiceApplication.class, args);
+	}
+
+}
