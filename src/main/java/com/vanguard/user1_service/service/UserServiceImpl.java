2d18aa6eead6358973fb66cc47bf5fab8e093f9d
abhijeet <abhijeet.thakur1@gmail.com>
2026-01-27 14:00:39 +0530

    feat: add get user by id api

--- src/main/java/com/vanguard/user1_service/service/UserServiceImpl.java
+++ src/main/java/com/vanguard/user1_service/service/UserServiceImpl.java
@@ -22,4 +22,9 @@ public class UserServiceImpl implements UserService {
 
         return userRepository.save(user);
     }
+
+    @Override
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found with id: " + id));    }
 }
