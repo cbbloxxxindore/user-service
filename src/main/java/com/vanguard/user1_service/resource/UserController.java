ab4bffb3d23dabc48e673aa961690f4f963d64c5
cbbloxxxindore <35773285+cbbloxxxindore@users.noreply.github.com>
2026-01-30 11:09:46 +0530

    logs added (#4)
    logs added in service ,resource and exception

--- src/main/java/com/vanguard/user1_service/resource/UserController.java
+++ src/main/java/com/vanguard/user1_service/resource/UserController.java
@@ -5,6 +5,7 @@ import com.vanguard.user1_service.models.User;
 import com.vanguard.user1_service.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,20 +13,37 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
 
     private final UserService userService;
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<User>  createUser(@Valid @RequestBody UserCreateRequest request) {
+    public ResponseEntity<User> createUser(
+            @Valid @RequestBody UserCreateRequest request) {
+
+        log.info("Create User API called");
+        log.debug("Create User request payload: name={}, email={}",
+                request.getName(), request.getEmail());
+
+        User user = userService.createUser(request);
+
+        log.info("User created successfully with id={}", user.getId());
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(userService.createUser(request));    }
+                .body(user);
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity<User> getUserById(@PathVariable Long id) {
-        return ResponseEntity.ok(userService.getUserById(id));
+
+        log.info("Get User API called with id={}", id);
+
+        User user = userService.getUserById(id);
+
+        log.info("User fetched successfully with id={}", id);
+
+        return ResponseEntity.ok(user);
     }
 }
